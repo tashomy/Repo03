3b9217503b186de29963b29240f4263f7510dcae
Natasa Mijatovic <mijatovicn650@gmail.com>
2021-09-15 18:54:17 +0200

    ex02 added

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+    //Only change code below this line
+
+    //one line comment
+
+    /*
+    Multi
+    line
+    comment
+    */
+
+    //Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;
